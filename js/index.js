15568f7815104f4035503f4205291a46935b6d83
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-08 00:10:50 -0700

    added some more event listeners

--- js/index.js
+++ js/index.js
@@ -3,16 +3,18 @@ const get = (selector) => {
     return document.querySelector(selector)
 }
 const header = get('header');
-const logoHeading = get('.logo-heading');
-const navContainerClass = get('.nav-container');
-const navContainer = get('.main-navigation .container')
+const funSizeReport = get('.logo-heading');
+const aTags = document.querySelectorAll('a');
 const busImage = get('.intro img');
 const ctaH2 = get('.intro h2');
+const allPs = document.querySelectorAll('p');
+const boatImageAdjust = get('.content-destination img');
 const buttonEls = document.querySelectorAll('.btn');
 let headerHasEventListener = true;
 let busFlipped = false;
+// console.log(funSizeReport);
 // header.setAttribute('style', 'zIndex:1');
-header.style.zIndex = '0';
+// header.style.zIndex = '0';
 function headerBackground(event){
     let headerHeight = header.clientHeight;
     let headerWidth = header.clientWidth;
@@ -35,8 +37,10 @@ function headerBackground(event){
 // header.addEventListener('mousemove', event => {
     //     console.log(`x is ${event.offsetX} y is ${event.offsetY};`);
     // })
-
-buttonEls[0].textContent = 'does nothing';
+function funSizeReporter(event){
+    funSizeReport.textContent = `window width is ${window.innerWidth}`;
+}
+// buttonEls[0].textContent = 'does nothing';
 buttonEls[1].textContent = 'toggle header color';
 function headerColorToggle(event){
     if (headerHasEventListener){
@@ -49,29 +53,41 @@ function headerColorToggle(event){
         headerHasEventListener = true;
     }
 }
+
+for(i = 0; i < aTags.length; i++){
+    aTags[i].addEventListener('click', function(event){
+        event.preventDefault();
+        event.stopPropagation();
+    })
+}
+// console.log(allPs[1].textContent);
+// function scrollAddWords(event){
+//     allPs[i].addEventListener('scroll', function(event) {
+//             allPs[i].textContent += 'Shane';
+//             debugger
+//         })
+// }
 // busImage.setAttribute('style', 'zIndex:0');
 // busImage.setAttribute('style', 'position:relative');
-
 function flipImage(event){
     if(busFlipped){
-        busImage.style.position = 'relative';
+        // busImage.style.position = 'relative';
         busImage.style.transform = 'rotate(0deg)';
-        busImage.style.zIndex = '0';
+        // busImage.style.zIndex = '0';
         // busImage.setAttribute('style','transform:rotate(0deg)');
         ctaH2.textContent = 'Welcome To Fun Bus!';
         busFlipped = false;
         // busImage.setAttribute('style', 'zIndex:0');
     }else {
         // busImage.setAttribute('style','transform:rotate(180deg)');
-        busImage.style.position = 'relative';
+        // busImage.style.position = 'relative';
         busImage.style.transform = 'rotate(180deg)';
-        busImage.style.zIndex = '0';
+        // busImage.style.zIndex = '0';
         ctaH2.textContent += ' in Australia??'
         busFlipped = true;
         // busImage.setAttribute('style', 'zIndex:0');
     }
 }
-
 // console.log(buttonEls);
 // console.log(buttonEls[1].textContent);
 // var box = document.querySelector(".box");
@@ -88,6 +104,10 @@ function flipImage(event){
 // signUpOne.addEventListener('click', event)
 // const buttonChange = get('.destination .btn'){
 // }
+// img#myImg {
+//     filter:invert(100%);
+//   }
+window.addEventListener('resize', funSizeReporter);
 header.addEventListener('mousemove', headerBackground);
 header.addEventListener('click', headerColorToggle);
 document.addEventListener('keydown', event =>{
@@ -95,5 +115,16 @@ document.addEventListener('keydown', event =>{
         header.style.backgroundColor = 'white';
     }
 });
+boatImageAdjust.addEventListener("mouseover", function( event ) {  
+    // debugger 
+    event.target.style.filter = "invert(100%)";
+    // setTimeout(function() {
+    //   event.target.style.filter = "";
+    // }, 1500);
+}, false);
+boatImageAdjust.addEventListener("mouseout", function( event ) {
+    event.target.style.filter = "";
+  }, false);
+// allPs[1].addEventListener('mouseover', scrollAddWords);
 buttonEls[1].addEventListener('click', headerColorToggle);
 busImage.addEventListener('click', flipImage);
